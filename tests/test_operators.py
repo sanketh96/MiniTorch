7c2de0218cf7e5e9677da4d68443472359721fb9
Sanketh Rangreji <sanketh.rangreji@logmein.com>
2022-03-13 13:21:39 +0530

    second commit

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -16,6 +16,7 @@ from minitorch.operators import (
     log_back,
     inv_back,
     sum,
+    is_close
 )
 from hypothesis import given
 from hypothesis.strategies import lists
@@ -105,7 +106,10 @@ def test_sigmoid(a):
     * it is  strictly increasing.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 0.0 <= sigmoid(a) <= 1.0
+    assert is_close(1.0 - sigmoid(a), sigmoid(-a))
+    assert sigmoid(0.5) > 0.0
+    assert sigmoid(a + 1.0) >= sigmoid(a)
 
 
 @pytest.mark.task0_2
@@ -113,39 +117,43 @@ def test_sigmoid(a):
 def test_transitive(a, b, c):
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if lt(a,b) and lt(b,c):
+        assert lt(a,c) == 1.0
 
 
 @pytest.mark.task0_2
-def test_symmetric():
+@given(small_floats, small_floats)
+def test_symmetric(a, b):
     """
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
     None
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert mul(a, b) == mul(b, a)
 
 
 @pytest.mark.task0_2
-def test_distribute():
+@given(small_floats, small_floats, small_floats)
+def test_distribute(x, y, z):
     r"""
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
     None
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(z, add(x,y)), add(mul(z,x), mul(z,y)))
 
 
 @pytest.mark.task0_2
-def test_other():
+@given(small_floats)
+def test_other(x):
     """
     Write a test that ensures some other property holds for your functions.
     """
     None
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert relu(x) >= x
 
 
 # ## Task 0.3  - Higher-order functions
